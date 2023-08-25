8fe8cd18d6aa670b9e4095b680f59aa62eeedd89
Wei Jin <410453494@qq.com>
2023-08-25 15:54:19 +0800

    tsg

--- card_analyze.py
+++ card_analyze.py
@@ -2,11 +2,38 @@
 import sys
 from toss import *
 
-valid_card = ['toss_up']
+valid_card = ['toss_up', 'tenshukaku', 'tsg']
+
+def avg(lst):
+    return sum(lst) / len(lst)
 
 def analyze_toss_up(dice_count, needs):
     test(needs, 0, dice_count)
 
+def analyze_tenshukaku(num_tsg, num_elem, init_omni=0, method='a'):
+    total_summary = []
+    effective_summary = []
+    for _ in range(TEST_COUNT):
+        if method == 'a':
+            dices = toss_aggresive(num_elem, init_omni)
+        else:
+            dices = toss_conservative(num_elem, init_omni)
+        
+        dice_type = dice_type_count(dices)
+        # Tenshukaku triggered
+        if dice_type >= 5:
+            dices[ELEM_OMNI] += num_tsg
+        # Count total & effective
+        total = sum(dices)
+        assert total == INIT_DICE_COUNT + init_omni or total == INIT_DICE_COUNT + init_omni + num_tsg, f'Dices: {dices}, total {total}, init_omni {init_omni}, num_tsg {num_tsg}'
+        total_summary.append(total)
+        effective_summary.append(dice_effective_count(dices, num_elem))
+    
+    if method == 'a':
+        print(f'Aggresive: Tenshukaku {num_tsg}, Effective elem type {num_elem}, Init omni {init_omni}, Average total {avg(total_summary)}, Effective total {avg(effective_summary)}')
+    else:
+        print(f'Conservative: Tenshukaku {num_tsg}, Effective elem type {num_elem}, Init omni {init_omni}, Average total {avg(total_summary)}, Effective total {avg(effective_summary)}')
+    
 if __name__ == '__main__':
     card = sys.argv[1].lower()
     
@@ -14,10 +41,19 @@ if __name__ == '__main__':
         print(f'Not valid card, available: {valid_card}')
         exit(1)
     
-    # toss_up
+    # Toss-Up
     if card == 'toss_up':
         dice_count = int(sys.argv[2])
         needs = [int(x) for x in sys.argv[3].split(',')]
     
-        print(f'Toss_up, with {dice_count} dices, needs: {needs}, run {TOSS_COUNT} times')
+        print(f'Toss_up, with {dice_count} dices, needs: {needs}, run {TEST_COUNT} times')
         analyze_toss_up(dice_count, needs)
+        
+    # Tenshukaku
+    if card == 'tenshukaku' or card == 'tsg':
+        for num_tsg in [0, 1, 2]:
+            for num_elem in [1, 2]:
+                for init_omni in [0, 1, 2]:
+                    for method in ['a', 'c']:
+                        analyze_tenshukaku(num_tsg, num_elem, init_omni, method=method)
+                    print('---------------------------------------------------------------------------------------------------')
--- toss.py
+++ toss.py
@@ -7,10 +7,32 @@ import random
 ELEM_TYPES = 8
 ELEM_OMNI = ELEM_TYPES - 1
 INIT_DICE_COUNT = 8
-TOSS_COUNT = 100000
+TEST_COUNT = 100000
+DICE_COUNT_UPPER_BOUND = 16
 
 def unaligned_range(needs):
     return range(len(needs), ELEM_OMNI)
+
+def init_dices(init_omni=0):
+    dices = [0] * INIT_DICE_COUNT
+    dices[ELEM_OMNI] += init_omni
+    return dices
+
+def dice_type_count(dices):
+    count = 0
+    for elem in dices:
+        if elem:
+            count += 1
+    if dices[ELEM_OMNI]:
+        count += dices[ELEM_OMNI] - 1
+    return count
+
+def dice_effective_count(dices, num_elem):
+    count = 0
+    for i in range(num_elem):
+        count += dices[i]
+    count += dices[ELEM_OMNI]
+    return count
 
 def dice_analyze(dices, needs):
     lack = 0
@@ -25,9 +47,9 @@ def dice_analyze(dices, needs):
         lack -= omni_count
     
     return lack
-    
-def first_toss(dices, dice_count=INIT_DICE_COUNT):
-    for _ in range(dice_count):
+
+def toss(dices, toss_count):
+    for _ in range(toss_count):
         elem = random.randrange(ELEM_TYPES)
         dices[elem] += 1
 
@@ -48,10 +70,57 @@ def conditional_toss(dices, needs):
             dices[idx] = x
     
     # print(f'dices before retoss: {dices}, # of retoss: {retoss_count}')
+    toss(dices, retoss_count)
+
+# For tenshukaku, Priority: keep 5 types of elems, may retoss effective elems if types not enough
+def toss_conservative(num_elem, init_omni=0):
+    dices = init_dices(init_omni)
+    toss(dices, INIT_DICE_COUNT)
+    # print(f'Dice before: {dices}')
     
-    for _ in range(retoss_count):
-        elem = random.randrange(ELEM_TYPES)
-        dices[elem] += 1
+    retoss_count = 0
+    types = dice_type_count(dices)
+    if types >= 5:
+        # Retoss redundant unaligned elems
+        keep_types = 5 - dices[ELEM_OMNI]
+        for i in range(num_elem):
+            # We have effective dices
+            if dices[i]:
+                keep_types -= 1
+        # Unaligned ones
+        for idx, dice in enumerate(dices[num_elem:-1], num_elem):
+            if dice:
+                if keep_types == 0:
+                    # Retoss all
+                    retoss_count += dice
+                    dices[idx] = 0
+                else:
+                    # Keep one
+                    retoss_count += dice - 1
+                    dices[idx] = 1
+                    keep_types -= 1
+    else:
+        # Keep one elem per type
+        for idx, dice in enumerate(dices[:-1]):
+            if dice:
+                retoss_count += dice - 1
+                dices[idx] = 1
+    # print(f'Dice after: {dices}, retoss count {retoss_count}')
+    
+    # Retoss
+    toss(dices, retoss_count)
+    
+    return dices
+
+# For tenshukaku
+def toss_aggresive(num_elem, init_omni=0):
+    dices = init_dices(init_omni)
+    toss(dices, INIT_DICE_COUNT)
+    needs = []
+    for _ in range(num_elem):
+        needs.append(DICE_COUNT_UPPER_BOUND)
+    conditional_toss(dices, needs)
+    return dices
 
 def test(needs, init_omni, dice_count=INIT_DICE_COUNT):
     total = 0
@@ -59,11 +128,10 @@ def test(needs, init_omni, dice_count=INIT_DICE_COUNT):
         total += x
     lacks = [0] * (total + 1)
     
-    for i in range(TOSS_COUNT):
-        dices = [0] * INIT_DICE_COUNT
-        dices[ELEM_OMNI] += init_omni
+    for i in range(TEST_COUNT):
+        dices = init_dices(init_omni)
         
-        first_toss(dices, dice_count)
+        toss(dices, dice_count)
         conditional_toss(dices, needs)
         
         lack = dice_analyze(dices, needs)
@@ -73,8 +141,8 @@ def test(needs, init_omni, dice_count=INIT_DICE_COUNT):
     expectation = 0
     for idx, x in enumerate(lacks):
         accum_prob += x
-        expectation += x * idx / TOSS_COUNT
-        print(f'Lack {idx} elems prob: {x / TOSS_COUNT}, accum prob: {accum_prob / TOSS_COUNT}')
+        expectation += x * idx / TEST_COUNT
+        print(f'Lack {idx} elems prob: {x / TEST_COUNT}, accum prob: {accum_prob / TEST_COUNT}')
     print(f'Expectation: {expectation}')
 
 if __name__ == '__main__':
@@ -83,5 +151,5 @@ if __name__ == '__main__':
         init_omni = int(sys.argv[2])
     else:
         init_omni = 0
-    print(f'Testing with needs: {needs}, run {TOSS_COUNT} times')
+    print(f'Testing with needs: {needs}, run {TEST_COUNT} times')
     test(needs, init_omni)
